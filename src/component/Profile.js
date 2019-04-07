32d1eb72f37217dbc9677f4f879f000244e9edc3
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-07 21:47:59 +0000

    required axios

---
+++ src/component/Profile.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+//uses component as the extends Component
+class Profile extends Component {
+	 state = {
+    artistname: null,
+    songname: null,
+    lyrics: null
+  }
+  
+  handleClick = () => {
+  	// console.log(this.state.firstname);
+  	// const artistName = document.getElementById("artist").value;
+  	// const songName = document.getElementById("song").value;
+  	
+  	const url = "https://api.lyrics.ovh/v1/" + this.state.artistname + "/" + this.state.songname;
+  	
+  	// console.log(url);
+  	axios.get(url)
+  	
+  	.then(response=>{
+  	// console.log(response);
+  		this.setState({
+  			lyrics:response.data.lyrics
+  		});
+  	});
+  }
+  
+  
+	render() {
+		return(
+			<div>
+				<p> My App </p>
+			
+			</div>
+			);
+	}
+}
+
+export default Profile;
