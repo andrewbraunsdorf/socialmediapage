8fffbcf1aff3220102147be11dd24e703b269503
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-07 21:54:34 +0000

    added setSongName Function nand input

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -3,45 +3,53 @@ import axios from 'axios';
 
 //uses component as the extends Component
 class Profile extends Component {
-	 state = {
-    artistname: null,
-    songname: null,
-    lyrics: null
-  }
-  
-  handleClick = () => {
-  	// console.log(this.state.firstname);
-  	// const artistName = document.getElementById("artist").value;
-  	// const songName = document.getElementById("song").value;
-  	
-  	const url = "https://api.lyrics.ovh/v1/" + this.state.artistname + "/" + this.state.songname;
-  	
-  	// console.log(url);
-  	axios.get(url)
-  	
-  	.then(response=>{
-  	// console.log(response);
-  		this.setState({
-  			lyrics:response.data.lyrics
-  		});
-  	});
-  }
-  
-    setArtistName = e => {
-  	// e.target.value;
-  	// console.log(e.target.value);
-  	this.setState({
-  		artistname: e.target.value
-  	});
-  }
-  
+	state = {
+		artistname: null,
+		songname: null,
+		lyrics: null
+	}
+
+	handleClick = () => {
+		// console.log(this.state.firstname);
+		// const artistName = document.getElementById("artist").value;
+		// const songName = document.getElementById("song").value;
+
+		const url = "https://api.lyrics.ovh/v1/" + this.state.artistname + "/" + this.state.songname;
+
+		// console.log(url);
+		axios.get(url)
+
+			.then(response => {
+				// console.log(response);
+				this.setState({
+					lyrics: response.data.lyrics
+				});
+			});
+	}
+
+	setArtistName = e => {
+		// e.target.value;
+		// console.log(e.target.value);
+		this.setState({
+			artistname: e.target.value
+		});
+	}
+
+	setSongName = e => {
+		this.setState({
+			songname: e.target.value
+		});
+	}
+
 	render() {
-		return(
+		return (
 			<div>
 				<p> My App </p>
 					<input onChange= {this.setArtistName} placeholder="Artist Name" />
+					<input onChange= {this.setSongName} placeholder="Song Name" />
+				<button onClick={this.handleClick}>Submit</button>
 			</div>
-			);
+		);
 	}
 }
 
